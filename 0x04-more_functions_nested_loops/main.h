0fbdc6ade4d7c32757e50ba6d370db0bd961f6c3
KNyatichi <pinkytichi@gmail.com>
2023-02-23 03:56:31 -0800

    Main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-nt _isupper(int c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -13,4 +13,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 #endif
-
