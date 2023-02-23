e99fd21b5ad27d4675ad4bb36e52fa06b8c39383
KNyatichi <123981863+KNyatichi@users.noreply.github.com>
2023-02-23 16:21:33 +0300

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,14 @@
  */
 void print_most_numbers(void)
 {
-int number;
-for (number = 48; number < 58; number++)
-{
-if ((number == 50) || (number == 52))
-{
-continue;
-}
-_putchar(number);
-}
-_putchar(10);
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number == '2' || number == '4')
+			;
+		else
+			_putchar(number);
+	}
+	_putchar('\n');
 }
