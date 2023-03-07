631f2358e801f00af4819648691f4891e93df1ec
KNyatichi <pinkytichi@gmail.com>
2023-03-06 23:22:23 -0800

    main.h file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
 
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char*c);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *strstr(char *haystack, char *needle);
