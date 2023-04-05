4a267ae930308ab7cbd1d22217b1aa9815d11287
KNyatichi <123981863+KNyatichi@users.noreply.github.com>
2023-04-05 16:01:32 +0300

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *curr;
 
-	if (head == NULL)
-	return;
-
-	while (*head)
-{
-	temp = (*head)->next;
-	free(*head);
-	*head = temp;
-}
-
-	*head = NULL;
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
