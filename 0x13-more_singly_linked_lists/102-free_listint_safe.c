71681c9a73f242b11bea5ff72a745817b154869e
KNyatichi <123981863+KNyatichi@users.noreply.github.com>
2023-04-05 21:28:46 +0300

    13. Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,36 +5,68 @@
  * @h: is a pointer to the first node in the linked list
  * Return: a number of elements from the freed list
  */
+
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
 
-	if (!h || !*h)
-	return (0);
+	hptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
 
-	while (*h)
-{
-	diff = *h - (*h)->next;
-	if (diff > 0)
-{
-	temp = (*h)->next;
-	free(*h);
-	*h = temp;
-	len++;
-}
-	else
-{
-	free(*h);
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
+		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
+	}
+
 	*h = NULL;
-	len++;
-	break;
+	free_listp2(&hptr);
+	return (nnodes);
 }
-}
-
-	*h = NULL;
-
-	return (len);
-}
-
