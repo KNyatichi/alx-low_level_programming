db2a6fea708e85e4b45b33ac925457ad5efab9d1
KNyatichi <123981863+KNyatichi@users.noreply.github.com>
2023-04-05 16:07:04 +0300

    8. Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,14 +7,14 @@
  */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
-	listint_t *temp = head;
+	int sum;
 
-	while (temp)
-{
-	sum += temp->n;
-	temp = temp->next;
-}
-	return (sum);
-}
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
+	return (sum);
+}
